4aa2484c5a79f643fe5455135633da14321f25fb
Jie Zhou <zhoujie@my.bridgeport.edu>
2016-10-20 12:55:48 -0400

    created entry class and hashtable class

--- Hash Table implementation.java
+++ Hash Table implementation.java
@@ -9,3 +9,20 @@ class LinkedHashEntry{
     this.next = null;
   }
 }
+
+class HashTable{
+  private int numOfBuckets;
+  private int size;
+  private LinkedHashEntry[] table;
+
+  HashTable(int buckets){
+    numOfBuckets = buckets;
+    size = 0;
+    table = new LinkedHashEntry[numOfBuckets];
+
+    for(int i = 0; i < numOfBuckets; i++){
+      table[i] = null;
+    }
+  }
+
+}
